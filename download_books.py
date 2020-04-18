c38930f4efdb195d696c76dc7d636639c3ec3afd
João André <joao.andre@mercadolivre.com>
2020-04-17 21:28:01 -0300

    Create folders if not exists and save books by category

--- download_books.py
+++ download_books.py
@@ -4,36 +4,29 @@ import requests
 import pandas as pd
 from urllib.parse import urlparse
 
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-invalid_chars = '<>:"/\|?*'.split()
-
+invalid_chars = '< > : " / \ | ? *'.split()
 
 session = requests.Session()
 df = pd.read_csv("books.csv")
 
 for i, book in df.iterrows():
-    file_name = ""
+    file_name = f'{book["English Package Name"]}/'
+    directory = book["English Package Name"]
+
     for c in book['Book Title']:
         file_name += '_' if c in invalid_chars else c
 
     file_name += '.pdf'
 
-    if os.path.isfile(file_name) :
+    if os.path.isfile(file_name):
         print(f'Ignoring {file_name}. Book already downloaded')
         continue
 
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
     urlObject = urlparse(book['DOI URL'])
-    url = f"https://link.springer.com/content/pdf/{urlObject.path}.pdf"
+    url = f"https://link.springer.com/content/pdf{urlObject.path}.pdf"
     print(f"Downloading {book['Book Title']} by {book['Author']} on URL {url}")
     print("-------------------------------------------------------------------")
     f = requests.get(url)
